a3ff937b832d6bf3b873f5193969f89311a78cf7
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2020-07-24 11:28:46 +0200

    Test to add JavaDoc 3

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"guerrerocraft61.particleapi"},{"l":"guerrerocraft61.particleapi.particletypes"}]
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"guerrerocraft61.particleapi.particletypes","l":"LineParticle"},{"p":"guerrerocraft61.particleapi","l":"ParticleAPI"}]
